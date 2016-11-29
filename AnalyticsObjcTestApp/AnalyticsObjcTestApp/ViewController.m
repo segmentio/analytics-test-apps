19aa9ee314ea730492b89970761b7fec343fb52e
Tony Xiao <tonyx.ca@gmail.com>
2016-11-28 21:36:12 -0800

    Initial commit with Objc Example

---
+++ AnalyticsObjcTestApp/AnalyticsObjcTestApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AnalyticsObjcTestApp
+//
+//  Created by Tony Xiao on 11/28/16.
+//  Copyright © 2016 Segment. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
