8378f3fc821262b8ecab4debeea4f7fcc9591575
Fathy <fathy.boundjadj@gmail.com>
2018-07-25 19:11:07 +0200

    Use `use_frameworks!` with Swift

--- AnalyticsIOSTestApp/AnalyticsIOSTestApp/AnalyticsIOSTestApp-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import <Analytics/SEGAnalytics.h>
-#import <Analytics/SEGAnalyticsConfiguration.h>
-#import <Analytics/SEGMiddleware.h>
-#import <Segment-Mixpanel/SEGMixpanelIntegrationFactory.h>
